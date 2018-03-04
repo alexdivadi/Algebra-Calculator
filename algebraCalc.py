e8c4694f43877c25f7cb8b055f5788f340a50479
David <mrbarkface513@gmail.com>
2018-03-03 19:36:36 -0800

    fixed constant iter bug

--- algebraCalc.py
+++ algebraCalc.py
@@ -12,46 +12,46 @@ class Variable():
     
     def combineTerms(self, array): #takes [6x, 4x] and makes it 10.0
         array = [i.split(self.var)[0] for i in array]
-        print(array)
-        print(sum(float(i) for i in array))
         return sum(float(i) for i in array)
+
+    def rejoin(self, total):
+        return str(total) + self.var
 
 
 #------methods------
 
 #Track variables -- ex. find x's
 def trackVar(var, varObjects, testArray, k):
+    skip = 0
     for x in range(len(testArray)):
         if testArray[x].isalpha(): #check if char is alphabetical
             if (testArray[x] in var) == False: #don't put same var in twice
-	        print(testArray[x],'is a variable.')
-                print(testArray[x])
                 a = x
                 b = testArray[x] #store char as variable
                 var.append(b)
                 a = Variable(b) #add an object to reference var later
                 varObjects.append(a)
         if testArray[x].isdigit(): #check if char is constant
-            print("There was a constant!")
-            i = x
-            print("i =",i)
-            while i <= len(testArray):
-                if testArray[i].isalpha():
-                    break
-                elif testArray[i].isdigit():
-                    i = i + 1
-                    try:
-                        testArray[i].isdigit()
-                    except IndexError:
-                        print("".join(testArray[x:(i)]))
-                        k.append("".join(testArray[x:(i)]))
-                        print(k)
+            if skip != 0:
+                skip = skip - 1
+                continue
+            else: 
+                i = x
+                while i <= len(testArray):
+                    if testArray[i].isalpha():
                         break
-                else:
-                    print("".join(testArray[x:(i)]))
-                    k.append("".join(testArray[x:(i)]))
-                    print(k)
-                    break
+                    elif testArray[i].isdigit():
+                        i = i + 1
+                        try:
+                            testArray[i].isdigit()
+                        except IndexError:
+                            k.append("".join(testArray[x:(i)]))
+                            skip = len("".join(testArray[x:(i)]))
+                            break
+                    else:
+                        k.append("".join(testArray[x:(i)]))
+                        skip = len("".join(testArray[x:(i)]))
+                        break
 #Combine Constants             
 def combineConstants(k):
     return (sum(float(i) for i in k))
@@ -64,13 +64,12 @@ def combineLikeTerms(test, con, variables):
     
     #split up characters
     testArray = list(test)
-    print(testArray)
-    
+
     trackVar(var, varObjects, testArray, k)
     
     #get rid of spaces
     test = test.split(" ")
-    print(test)
+
     #combine like terms
     con.append(combineConstants(k))
     for i in range(len(varObjects)):
@@ -85,20 +84,18 @@ leftvar = []
 rightcon = []
 rightvar = []
 test = raw_input("Enter an equation: ")
-print(test)
 testArray = test.split(" = ")
-print(testArray)
 
 exp = testArray[0]
 combineLikeTerms(exp, leftcon, leftvar)
 exp = testArray[1]
 combineLikeTerms(exp, rightcon, rightvar)
 print("Left side of equation:")
-print("    Variables:", leftvar)
-print("    Constants:", leftcon)
+print("    Variables: %s" % leftvar)
+print("    Constants: %s" % leftcon)
 print("Right side of equation:")
-print("    Variables:", rightvar)
-print("    Constants:", rightcon)
+print("    Variables: %s" % rightvar)
+print("    Constants: %s" % rightcon)
 
 
 
